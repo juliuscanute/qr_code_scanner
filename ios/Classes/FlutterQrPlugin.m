3edb6eeb7f8a3e9974c7a94d132d284c970376de
Leon <leondevlifelog@gmail.com>
2019-03-29 20:38:56 +0800

    fix #2 #3

--- ios/Classes/FlutterQrPlugin.m
+++ ios/Classes/FlutterQrPlugin.m
@@ -1,5 +1,5 @@
 #import "FlutterQrPlugin.h"
-#import "qr_code_scanner-Swift.h"
+#import "qr_code_scanner/qr_code_scanner-Swift.h"
 @implementation FlutterQrPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
     [SwiftFlutterQrPlugin registerWithRegistrar:registrar];
