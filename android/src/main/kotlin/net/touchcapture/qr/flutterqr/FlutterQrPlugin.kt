juliuscanute/qr_code_scanner

commit f05ac085b9ef14c86cb4eade7cf2323b139569db

cqa: code cleanup and refactor on android
- Remove usage of double bang operator
- Remove registered callbacks onDispose
- Code Refactor
- Reduce Magic Strings with Constants
- Create code regions
- Remove caching hasCameraPermission
- Check for camera permission on Activity Resume
- Create class to help with activity lifecycle

android/src/main/kotlin/net/touchcapture/qr/flutterqr/FlutterQrPlugin.kt
@@ -4,42 +4,44 @@ import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import net.touchcapture.qr.flutterqr.Shared.activity
 
 class FlutterQrPlugin : FlutterPlugin, ActivityAware {
 
     /** Plugin registration embedding v2 */
     override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        if (activity != null) {
-            activity = activity
-        }
         flutterPluginBinding.platformViewRegistry
             .registerViewFactory(
-                "net.touchcapture.qr.flutterqr/qrview", QRViewFactory(flutterPluginBinding.binaryMessenger))
+                VIEW_TYPE_ID,
+                QRViewFactory(flutterPluginBinding.binaryMessenger)
+            )
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        QrShared.activity = null
+        QrShared.binding = null
     }
 
     override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {
-        activity = activityPluginBinding.activity
-        Shared.binding = activityPluginBinding
+        QrShared.activity = activityPluginBinding.activity
+        QrShared.binding = activityPluginBinding
     }
 
     override fun onDetachedFromActivityForConfigChanges() {
-        activity = null
-        Shared.binding = null
+        QrShared.activity = null
+        QrShared.binding = null
     }
 
     override fun onReattachedToActivityForConfigChanges(activityPluginBinding: ActivityPluginBinding) {
-        activity = activityPluginBinding.activity
-        Shared.binding = activityPluginBinding
+        QrShared.activity = activityPluginBinding.activity
+        QrShared.binding = activityPluginBinding
     }
 
     override fun onDetachedFromActivity() {
-        activity = null
-        Shared.binding = null
+        QrShared.activity = null
+        QrShared.binding = null
     }
-    
 
+    companion object {
+        private const val VIEW_TYPE_ID = "net.touchcapture.qr.flutterqr/qrview"
+    }
 }

android/src/main/kotlin/net/touchcapture/qr/flutterqr/CustomFramingRectBarcodeView.kt
@@ -9,8 +9,8 @@ import com.journeyapps.barcodescanner.Size
 class CustomFramingRectBarcodeView : BarcodeView {
     private var bottomOffset = BOTTOM_OFFSET_NOT_SET_VALUE
 
-    constructor(context: Context?) : super(context) {}
-    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {}
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(
         context,
         attrs,

android/src/main/kotlin/net/touchcapture/qr/flutterqr/QRViewFactory.kt
@@ -7,13 +7,24 @@ import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
 
-class QRViewFactory(private val messenger: BinaryMessenger) :
-        PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class QRViewFactory(
+    private val messenger: BinaryMessenger
+) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
 
-    override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
+    override fun create(
+        context: Context?,
+        viewId: Int,
+        args: Any?
+    ): PlatformView {
+
         @Suppress("UNCHECKED_CAST")
         val params = args as HashMap<String, Any>
 
-        return QRView(requireNotNull(context), messenger, viewId, params)
+        return QRView(
+            context = requireNotNull(context),
+            id = viewId,
+            messenger = messenger,
+            params = params
+        )
     }
 }

android/src/main/kotlin/net/touchcapture/qr/flutterqr/QrActivityLifecycleCallbacks.kt
@@ -1,0 +1,41 @@
+package net.touchcapture.qr.flutterqr
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+class UnRegisterLifecycleCallback(
+    private val application: Application,
+    private val callback: Application.ActivityLifecycleCallbacks,
+) {
+    operator fun invoke() = application.unregisterActivityLifecycleCallbacks(callback)
+}
+
+fun Activity.registerLifecycleCallbacks(
+    onPause: (() -> Unit)? = null,
+    onResume: (() -> Unit)? = null,
+): UnRegisterLifecycleCallback {
+    val callback = object : Application.ActivityLifecycleCallbacks {
+        override fun onActivityPaused(p0: Activity) {
+            if (p0 == this@registerLifecycleCallbacks) onPause?.invoke()
+        }
+
+        override fun onActivityResumed(p0: Activity) {
+            if (p0 == this@registerLifecycleCallbacks) onResume?.invoke()
+        }
+
+        override fun onActivityStarted(p0: Activity) = Unit
+
+        override fun onActivityDestroyed(p0: Activity) = Unit
+
+        override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) = Unit
+
+        override fun onActivityStopped(p0: Activity) = Unit
+
+        override fun onActivityCreated(p0: Activity, p1: Bundle?) = Unit
+    }
+
+    application.registerActivityLifecycleCallbacks(callback)
+
+    return UnRegisterLifecycleCallback(application, callback)
+}

android/src/main/kotlin/net/touchcapture/qr/flutterqr/QrShared.kt
@@ -1,11 +1,15 @@
 package net.touchcapture.qr.flutterqr
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.PluginRegistry
 
-object Shared {
+@SuppressLint("StaticFieldLeak")
+object QrShared {
     const val CAMERA_REQUEST_ID = 513469796
+
     var activity: Activity? = null
+
     var binding: ActivityPluginBinding? = null
+
 }

commit f5c0c5a973e67ebdb24769365e0de15ba129739a

Update FlutterQrPlugin.kt
Fixing null errors resulting from detachment from engine introduced in commit f05ac08

android/src/main/kotlin/net/touchcapture/qr/flutterqr/FlutterQrPlugin.kt
@@ -17,8 +17,8 @@ class FlutterQrPlugin : FlutterPlugin, ActivityAware {
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        QrShared.activity = null
-        QrShared.binding = null
+        // Leave empty
+        // Nullifying QrShared.activity and QrShared.binding here will cause errors if plugin is detached by another plugin
     }
 
     override fun onAttachedToActivity(activityPluginBinding: ActivityPluginBinding) {